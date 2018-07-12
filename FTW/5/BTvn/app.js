f604843d5c6ac89c4077128b82ecbfa06622cc1f
= <=>
2018-07-12 17:13:15 +0700

    complete ex

--- FTW/5/BTvn/app.js
+++ FTW/5/BTvn/app.js
@@ -4,43 +4,78 @@ var hbs=require('express-handlebars')
 var server=require("http").createServer(app)
 server.listen(4000)
 var io=require("socket.io").listen(server);
-
+const questionList=require('./question.json')//lay file json
+var fs=require("fs")
 app.use(express.static("public"))
-
+const bodyParser=require("body-parser")
 app.engine("handlebars",hbs({defaultLayout:"main"}))
 app.set('view engine',"handlebars");
 
-var arrQs=["aaa","bbb","ccc","ddd","eee"];
+app.use(bodyParser.urlencoded({extended:false}));
+
 
 app.get('/qs',function(req,res){
     res.render('qs')
 })
 app.get(["/","/ans"],function(req,res){
+    let rand=Math.floor(Math.random()*questionList.length)
+    let question =questionList[rand];
     res.render('ans',{
-        qs:arrQs[Math.floor(Math.random()*arrQs.length)]
+        qs:questionList[rand],
+        question
+    })
+
+})
+app.get("/question/:questionId",function(req,res){
+    let question =questionList[req.params.questionId];
+    var left,right;
+    if(question.no==0&&question.yes==0){
+        left="50.00%";
+        right="50.00%";
+    }
+    else{
+        left=Math.round((question.no*100/(question.yes+question.no)) * 100) / 100+"%";
+        right=Math.round((question.yes*100/(question.yes+question.no)) * 100) / 100+"%";
+    }
+    res.render("vote",{
+        question,
+        left,
+        right,
+        totalVote:question.yes+question.no
     })
 })
-// app.get('/',function(req,res){
-//     res.render('ans',{
-//         qs:arrQs[Math.floor(Math.random()*arrQs.length)],
+
+app.get("/answer/:questionId/:vote",(req,res)=>{
+    questionList[req.params.questionId][req.params.vote]+=1;
+    fs.writeFileSync("./question.json",JSON.stringify(questionList));
+    res.redirect("/question/"+req.params.questionId)
+})
+
+// for(let i=0;i<questionList.length;i++){
+//     app.get('/question'+i,function(req,res){
+//         res.render('vote',{
+//             qs:questionList[i]
+//         })
 //     })
-// })
-for(let i=0;i<arrQs.length;i++){
-    app.get('/question'+i,function(req,res){
-        res.render('vote',{
-            qs:arrQs[i]
-        })
-    })
-}
+// }
 
 
 
-io.on("connection",function(socket){
-    socket.on("Client-click-other-qs",function(){
-        socket.emit("server-send-qs",arrQs);
-    })
-
-    socket.emit("server-send-qs2",arrQs);
-
-
+app.post("/question/add",(req,res)=>{
+    console.log(req.body)
+    let newQs={content:req.body.questionContent,yes:0,no:0,id:questionList.length}
+    questionList.push(newQs)
+    fs.writeFileSync("./question.json",JSON.stringify(questionList));
+    res.redirect("/question/"+newQs.id)
+    
 })
+
+// io.on("connection",function(socket){
+//     socket.on("Client-click-other-qs",function(){
+//         socket.emit("server-send-qs",questionList);
+//     })
+
+//     // socket.emit("server-send-qs2",arrQs);
+
+
+// })
--- FTW/5/BTvn/public/index.js
+++ FTW/5/BTvn/public/index.js
@@ -1,36 +1,45 @@
 
 $(document).ready(function(){
-    var socket=io("http://localhost:4000");
-    $("#otherQs").click(function(){
-        socket.emit("Client-click-other-qs")
-        socket.on("server-send-qs",function(data){
-            do{
-                var text=data[Math.floor(Math.random()*data.length)]
-                var texthtml=$("#qsBlock").text()
-                console.log(texthtml+" "+text);
-                console.log("")
-                if(texthtml!=text){
-                    console.log("khac")
-                    $("#qsBlock").html(text)
-                    console.log($("#qsBlock").text())
-                }
-            }
-            while(texthtml==text)
+    // var socket=io("http://localhost:4000");
+    // $("#otherQs").click(function(){
+    //     socket.emit("Client-click-other-qs")
+    //     socket.on("server-send-qs",function(data){
+    //         do{
+    //             var text=data[Math.floor(Math.random()*data.length)].content
+    //             var texthtml=$("#qsBlock").text()
+    //             console.log(texthtml+" "+text);
+    //             console.log("")
+    //             if(texthtml!=text){
+    //                 console.log("khac")
+    //                 $("#qsBlock").html(text)
+    //                 console.log($("#qsBlock").text())
+    //             }
+    //         }
+    //         while(texthtml==text)
             
-        })
-        socket.on("server-send-qs2",function(data){
-            var text=$("#qsBlock").text();
-            var index=data.indexOf(text);
-            document.getElementById("votebtn").href = "http://localhost:4000/question"+index;
-        })
-    })
+    //     })
+    //     // socket.on("server-send-qs2",function(data){
+    //     //     var text=$("#qsBlock").text();
+    //     //     var index=data.indexOf(text);
+    //     //     document.getElementById("votebtn").href = "http://localhost:4000/question"+index;
+    //     // })
+    // })
     
-    socket.on("server-send-qs2",function(data){
-        var text=$("#qsBlock").text();
-        var index=data.indexOf(text);
-        document.getElementById("votebtn").href = "http://localhost:4000/question"+index;
-    })
+    // socket.on("server-send-qs2",function(data){
+    //     var text=$("#qsBlock").text();
+    //     var index=data.indexOf(text);
+    //     document.getElementById("votebtn").href = "http://localhost:4000/question"+index;
+    // })
 
-    
-    
+    document.getElementById("text-area-qs").onkeyup = function() {
+        var content=document.getElementById("text-area-qs").value;
+        var after=200-content.length;
+        document.getElementById("count-down-letter").innerHTML=after;
+    };
+    document.getElementById("text-area-qs").onchange = function() {
+        var content=document.getElementById("text-area-qs").value;
+        var after=200-content.length;
+        document.getElementById("count-down-letter").innerHTML=after;
+    };
+   
  });
